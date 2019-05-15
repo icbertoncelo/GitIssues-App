60489a4df331fe1ede756be9ad359c05fb4cfb56
Ian Carlos <carlos.ian007@gmail.com>
2019-05-15 11:01:30 -0300

    Feat(structure): Add and configure the project structure

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import './config/ReactotronConfig';
+import './config/DevToolsConfig';
+
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const App = () => (
+  <View>
+    <Text>Welcome</Text>
+  </View>
+);
+
+export default App;
